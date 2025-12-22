cdc8265798c2ff555a6ccbb8e7215d1ad99c3235
sakethmurari <murarisaketh@gmail.com>
2025-12-22 18:53:07 +0530

    week1

---
+++ personal_intro.py
@@ -0,0 +1,8 @@
+# Personal Introduction Program
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+hobby = input("What is your favorite hobby? ")
+
+print(f"🎉 Welcome {name} 🎉")
+print(f"You are {age} years old and love Coding.")
